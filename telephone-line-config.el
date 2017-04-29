d594052b42e70247f27bd4381777b56be5686a6c
Daniel Bordak <dbordak@fastmail.fm>
2017-04-29 15:36:37 -0400

    Add easy evil setup

---
+++ telephone-line-config.el
@@ -0,0 +1,44 @@
+;;; telephone-line-config.el --- Easy config for telephone-line
+
+;; Copyright (C) 2015-2017 Daniel Bordak
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Easy, premade config(s) for telephone-line.
+
+;;; Code:
+
+(require 'telephone-line)
+
+(defun telephone-line-evil-config ()
+  "A simple default for using telephone-line with evil."
+  (setq telephone-line-lhs
+        '((evil   . (telephone-line-evil-tag-segment))
+          (accent . (telephone-line-vc-segment
+                     telephone-line-erc-modified-channels-segment
+                     telephone-line-process-segment))
+          (nil    . (telephone-line-minor-mode-segment
+                     telephone-line-buffer-segment))))
+
+  (setq telephone-line-rhs
+        '((nil    . (telephone-line-misc-info-segment))
+          (accent . (telephone-line-major-mode-segment))
+          (evil   . (telephone-line-airline-position-segment))))
+
+  (telephone-line-mode t))
+
+(provide 'telephone-line-config)
+;;; telephone-line-config.el ends here
