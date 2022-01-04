547979a092253e77a712a87ebec11135984711ad
Stefan Kangas <stefan@marxist.se>
2022-01-04 05:58:47 +0100

    Update copyright years

--- telephone-line-utils.el
+++ telephone-line-utils.el
@@ -1,6 +1,6 @@
 ;;; telephone-line-utils.el --- Functions for defining segparators and segments -*- lexical-binding: t -*-
 
-;; Copyright (C) 2015-2017 Daniel Bordak
+;; Copyright (C) 2015-2022 Daniel Bordak
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
--- telephone-line.el
+++ telephone-line.el
@@ -1,6 +1,6 @@
 ;;; telephone-line.el --- Rewrite of Powerline -*- lexical-binding: t -*-
 
-;; Copyright (C) 2015-2017 Daniel Bordak
+;; Copyright (C) 2015-2022 Daniel Bordak
 
 ;; Author: Daniel Bordak <dbordak@fastmail.fm>
 ;; URL: https://github.com/dbordak/telephone-line
